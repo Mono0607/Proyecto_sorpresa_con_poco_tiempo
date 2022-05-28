d261e3da267d6bce4eb6eabd98ba7613d7d65070
Natdrags <98502921+Natdrags@users.noreply.github.com>
2022-05-28 17:29:17 -0500

    continuacion tablas

--- app/src/main/java/com/example/proyecto_sorpresa_con_poco_tiempo/tablas2.kt
+++ app/src/main/java/com/example/proyecto_sorpresa_con_poco_tiempo/tablas2.kt
@@ -1,59 +1,77 @@
 package com.example.proyecto_sorpresa_con_poco_tiempo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import kotlin.random.Random
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+var respuestaCorrecta = false
 
-/**
- * A simple [Fragment] subclass.
- * Use the [tablas2.newInstance] factory method to
- * create an instance of this fragment.
- */
 class tablas2 : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var tvFactor1: TextView
+    lateinit var tvFactor2: TextView
+    lateinit var etRespuesta: EditText
+    lateinit var btnVerificar: Button
+    var factor1:Int = 0
+    var factor2:Int = 0
+    var producto:Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tablas2, container, false)
+
+        val ventana =  inflater.inflate(R.layout.fragment_tablas2, container, false)
+        fun generaMultiplicacion(){
+            factor1 = Random.nextInt(0,10)
+            factor2 = Random.nextInt(0,10)
+            producto = factor1*factor2
+            tvFactor1.text = "$factor1"
+            tvFactor2.text = "$factor2"
+            etRespuesta.text.clear()
+
+        }
+        fun initUI(){
+            tvFactor1 = ventana.findViewById(R.id.tvFactor1)
+            tvFactor2 = ventana.findViewById(R.id.tvFactor2)
+            etRespuesta = ventana.findViewById(R.id.etRespuesta)
+            btnVerificar = ventana.findViewById(R.id.btnVerificar)
+
+            generaMultiplicacion()
+
+        }
+
+
+        initUI()
+
+        btnVerificar.setOnClickListener {
+            var strRespuesta = etRespuesta.text.toString()
+            if(strRespuesta.equals("")){
+                Toast.makeText(requireContext(), "Respuesta Vacia", Toast.LENGTH_SHORT).show()
+
+            }else{
+                if (producto==strRespuesta.toInt()){
+
+                    respuestaCorrecta=true
+                }
+                else{
+                    respuestaCorrecta=false
+                }
+                findNavController().navigate(R.id.action_tablas2_to_tablas3)
+            }
+
+        }
+        return ventana
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment tablas2.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            tablas2().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
